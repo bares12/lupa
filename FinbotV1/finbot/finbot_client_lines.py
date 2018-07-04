0c60230b1b58b937c84a078fd640c3e212a7fd7b
ALFINONH <39095075+ALFINONH@users.noreply.github.com>
2018-07-04 16:03:04 +0700

    Update finbot_client_lines.py

--- FinbotV1/finbot/finbot_client_lines.py
+++ FinbotV1/finbot/finbot_client_lines.py
@@ -6,5 +6,5 @@ wait/"linux","cantos","windows","FinbotV1","vultra"/import .finbot or .finbotpy 
 .finbot import database.server
 from .finbot, ranger, response, FinbotServer, types, rangerv2, finbotV.04
 import updater from types..FinbotServer..finbot
-from LINE.DEVELOVER import finbotpy
+from LINE.DEVELOPER import finbotpy
 imposible UTF.8 UIX to PYTHON3
