cb24669ed316db4fe2d697885c12386f94d09b87
ALFINONH <39095075+ALFINONH@users.noreply.github.com>
2018-07-04 16:02:07 +0700

    Update finbot_client_lines.py

--- FinbotV1/finbot/finbot_client_lines.py
+++ FinbotV1/finbot/finbot_client_lines.py
@@ -1,10 +1,10 @@
 restarting...
 pointer...
 server into vps
-wait/"linux","cantos","windows","FinbotV1","vultra"/import FinbotV1.finbot or FinbotV1.finbotpy expel FinbotServer=11 into file microscofe.txt for database
-FinbotV1.finbot import wait poin.services
-FinbotV1.finbot import database.server
-from FinbotV1.finbot, ranger, response, FinbotServer, ttypes, rangerv2, finbotV.04
-import updater from ttypes..FinbotServer..finbot
+wait/"linux","cantos","windows","FinbotV1","vultra"/import .finbot or .finbotpy expel FinbotServer=11 into file microscofe.txt for database
+.finbot import wait poin.services
+.finbot import database.server
+from .finbot, ranger, response, FinbotServer, types, rangerv2, finbotV.04
+import updater from types..FinbotServer..finbot
 from LINE.DEVELOVER import finbotpy
 imposible UTF.8 UIX to PYTHON3
